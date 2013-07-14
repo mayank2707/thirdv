548e9b944cd4af2b8b05dd4c04e45ae52419f6fe
Rahul Jayaraman <recklessrahul@gmail.com>
2013-07-14 10:20:03 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightTraining::Application.config.secret_token = '6d6ffeb6c5a16244b09262516301f29a12e6035f7e8949523bc2635f1dde836c238660fc0a7c4a7ceb9412c660d9768cd83af3b6d9f13a85cee867fe673d4a76'
