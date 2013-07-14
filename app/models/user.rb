d1999dcba97fca35beb7f1ed9289690da8bd9004
Rahul Jayaraman <recklessrahul@gmail.com>
2013-07-14 10:20:19 +0530

    Authentication with sorcery.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+  attr_accessible :username, :email, :password, :password_confirmation
+  validates_length_of :password, :minimum => 5, :message => "password must be at least 5 characters long", :if => :password
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+  validates_uniqueness_of :username, :email
+end
